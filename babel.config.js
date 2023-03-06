324cc29e69ad37b0f9a46de6c8b5043659fc8810
Shehryar Raza <42263739+RazaShehryar@users.noreply.github.com>
2023-03-06 14:54:35 +0500

    setup module resolver for module alias

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,28 @@
-module.exports = function(api) {
-  api.cache(true);
+module.exports = function (api) {
+  api.cache.using(() => process.env.NODE_ENV);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      ["@babel/plugin-proposal-export-namespace-from"],
+      [
+        "module-resolver",
+        {
+          // extensions: ['.ts', '.tsx', '.js', '.jsx'],
+          alias: {
+            assets: "./assets",
+            components: "./src/components",
+            context: "./src/context",
+            helpers: "./src/helpers",
+            hooks: "./src/hooks",
+            navigation: "./src/navigation",
+            screens: "./src/screens",
+            static: "./src/static",
+            styles: "./src/styles",
+            utils: "./src/utils",
+          },
+        },
+      ],
+      "react-native-reanimated/plugin",
+    ],
   };
 };
